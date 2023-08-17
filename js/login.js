0bbaf6656f49dc578527c4f3e19423a882871c06
jahir0033 <mdjahirulislam0033@gmail.com>
2023-08-17 15:26:02 +0600

    added email and password

---
+++ js/login.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-submit').addEventListener('click',function(){
+  const emailField =document.getElementById('email-field');
+  const email =emailField.value;
+
+  const passwordField =document.getElementById('user-password');
+  const password =passwordField.value;
+  console.log(email,password);
+})
